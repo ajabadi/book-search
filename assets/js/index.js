04b112a303f8db7a886a8471bf9782c740ae95fb
EmpireAntz <thebigboognish@gmail.com>
2023-10-30 21:13:10 -0700

    added a function to display a random quote on our index html page using api ninjas quotes api. on page refresh a new quote should be rendered each time. we also added error handling in case there is a bad request from the server.

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,8 @@ var gBooksAPIKey = "AIzaSyAB-DMWo1SEDPqiD8Ihs-wgBnfsUTn9DRo"
 var searchForm = document.getElementById('search-form')
 //Selects the text input field where the user will enter their unput
 var userInput = document.getElementById('book-input-field')
+//Calls our function to display random quotes on the inex.html
+displayQuote()
 //Adds an event listener of submit to the entire search form
 searchForm.addEventListener('submit', function (e) {
     //Gets the value entered by the user in the input field
@@ -47,24 +49,49 @@ function searchBooks(input) {
 }
 
 
-/*Add in api ninja quotes url and key to use for 
-display a random quote on the starting page
+//Function to display random quotes on the inex.html
+function displayQuote() {
+    //Varibale to store our category for the quotes api to use
+    var category = "education"
+    //Fetches our url with the applied parameters
+    fetch('https://api.api-ninjas.com/v1/quotes?category=' + category, {
+        method: 'GET',
+        headers: {
+            'X-Api-Key': '9xPjrDWw0/aH+SruJ0tRJw==24YxIRpIMy6xeVRc',
+            'Content-Type': 'application/json'
+        },
+    })
+    //Requests a response from the server
+    .then(function(resp){
+        //If the response status is not an OK code, an error is thrown and caught at the end of the response chain
+        if (!resp.ok) {
+            throw new Error(`HTTP error! Status: ${resp.status}`)
+        }
+        //Gets the response in the form of json data
+        return resp.json()
+    })
+    //Gets the data we return from our first then
+    .then(function(data){
+        //Log the data to be able to traverse
+        console.log(data)
+        //Gets the element where the quote will be appended
+        var quotePlacement = document.getElementById('quote-placement')
+        //Gets the area where the quote author will be appended
+        var quoteAuthorPlacement = document.getElementById('quote-author')
+        //Gets the author info from our returned json data
+        var author = data[0].author
+        //Gets the quote info from our returned json data
+        var quote = data[0].quote
+        //Appends the random quote to the html
+        quotePlacement.textContent = '"' + quote + '"'
+        //Appends the quotes author to the html
+        quoteAuthorPlacement.textContent = "-" + author
+    })
+    //Will catch our above error if the response is not OK
+    .catch (function(err){
+        console.error(err, "Could not fetch data")
+    })
+ 
 
-Need to handle error for when user doesnt input any data
+} 
 
-On form submit we should be redirected to a new html that has cards 
-displayed in 2 columns
-
-new html file needs to be created to display the searched books
-in the form of cards.
-cards should be displayed dynamicially with js and styled 
-with materialize
-
-Cards should display book title, cover, author, and description
-related to user input
-
-once application is fully functional, add cutom css to stylize
-
-
-
-*/
