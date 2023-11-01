523fef0d22f71212dd773ecb729eecce84c6cf22
EmpireAntz <thebigboognish@gmail.com>
2023-10-31 20:19:52 -0700

    added a way to add saved books into local storage by clicking on the cards save button. when the user saves a book they can go to their favorites page and view the books they saved.

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -1,1 +1,37 @@
-console.log('youve reached the favorites page')
+
+var favorites = JSON.parse(localStorage.getItem('favorites'))
+console.log(favorites)
+
+document.addEventListener('DOMContentLoaded', function() {
+    var cardContainer = document.getElementById('card-container')
+    var cardsHTML = ''
+
+    favorites.forEach(function(book) {
+        var bookTitle = book.volumeInfo.title
+        //Gets the description for each of the books
+        var bookDescription = book.volumeInfo.description
+        //Gets the author for each of the books
+        var bookAuthor = book.volumeInfo.authors
+        //Gets the cover art for each book
+        var bookThumbnail = book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail
+
+        cardsHTML += `
+        <div class="row">
+            <div class="col s12 m12">
+                <div class="card-panel teal lighten-1">
+                    <img src="${bookThumbnail}">
+                    <a class="btn-floating btn-medium waves-effect waves-light right library"><i class="material-icons">clear</i></a>
+                    <span class="black-text">
+                        <h3>${bookTitle}</h3>
+                        <h5>-${bookAuthor}</h5>
+                        <p>${bookDescription}</p>
+                    </span>
+                </div>
+            </div>
+        </div>
+
+        `    
+        cardContainer.innerHTML = cardsHTML
+    })
+    
+})
